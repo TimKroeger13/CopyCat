f0378edbdde3b0da2f219be7c43dfd4af524058e
TimKroeger13 <78364349+TimKroeger13@users.noreply.github.com>
2026-03-22 21:04:01 +0100

    New try

--- CopyCat.Server/Program.cs
+++ CopyCat.Server/Program.cs
@@ -5,7 +5,7 @@ builder.Services.AddTransient<IButtonHub, ButtonHub>();
 builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(policy =>
-        policy.WithOrigins("http://localhost:5500", "http://127.0.0.1:5500", "https://tkroeger.com/", "http://tkroeger.com/")
+        policy.WithOrigins("http://localhost:5500", "http://127.0.0.1:5500", "https://tkroeger.com", "http://tkroeger.com")
               .AllowAnyHeader()
               .AllowAnyMethod()
               .AllowCredentials());
